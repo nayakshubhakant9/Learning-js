c515115cb49dcf167f7c9e2f1f875af3b7fce92b
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-07-08 17:46:53 +0530

    Data types

---
+++ 01_BASICS/02_DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treate all js code as a newer version
+// alert (3+3) , we are not using brouser , using node js
+
+console.log(3 + 3); // code readeability should be high
+
+console.log(shubhakant);
+
+let name = "shubhakant";
+let age = 20;
+let isTogggleIn = false;
+let state;
+
+/* number => 2 to power 53
+ bigint
+ string => ""
+ boolean => true/false
+ null => standalone value
+ undefined => 
+ symbol => unique
+
+ object
+
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object , this is an errror of the language
