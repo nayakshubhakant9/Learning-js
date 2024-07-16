330285dff9c44437b8c0fd16c07dfd9c82c01867
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-07-16 16:42:26 +0530

    conversion of datatypes

---
+++ 01_BASICS/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "shubhakant"
+//console.log(typeof score); // passing the upper value
+//console.log(score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN = Not a number 
+// true => 1; false => 0
+
+let isLoggedIn = "shubhakant"
+
+// let booleanIsLoggedin = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedin);
+
+// 1 => true; 0 => false
+// "" => false
+// "shubhakant" => true
+
+let someNumber = 33
+
+// let stringNumber = String(someNumber)
+//  console.log(stringNumber);
