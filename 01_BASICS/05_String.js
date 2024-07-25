750d2b4266240c62b1aee188f526cf1e0fc6e438
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-07-25 16:48:11 +0530

    Methods

--- 01_BASICS/05_String.js
+++ 01_BASICS/05_String.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 //console.log(name + repoCount + "value"); // this is not a good practice
 
-console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+//console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
 
 const gameName = new String('shubhakant-sk-com')
 
@@ -12,23 +12,30 @@ const gameName = new String('shubhakant-sk-com')
 
 //  console.log(gameName.length);
  // console.log(gameName.toUpperCase());
- console.log(gameName.charAt(2));
- console.log(gameName.indexOf('u'));
+ //console.log(gameName.charAt(2));
+ //console.log(gameName.indexOf('u'));
 
 const newString = gameName.substring(0,4)
-console.log(newString);
+//console.log(newString);
 
 const anotherString = gameName.slice(-12, 4) // for reverse value
-console.log(anotherString);
+//console.log(anotherString);
 
 const newStringOne = "    shubhakant   "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
 
 const url = "https://shubhakant.com%20nayak"
 
-console.log(url.replace('%20', '-'));
+// console.log(url.replace('%20', '-'));
 
-console.log(url.includes('shub'));
+// console.log(url.includes('shub'));
 
-console.log(gameName.split('-'));
+// console.log(gameName.split('-'));
+
+const String1 = 'Shubhakant'
+const String2 = 'Nayak'
+ console.log(String1.concat(' ',String2));
+ console.log(String2.concat(' ',String1));
+
+ 
