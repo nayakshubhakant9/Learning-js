18e490385617225a8c588c9af591f44af8a7f087
SHUBHAKANT NAYAK <146442931+nayakshubhakant9@users.noreply.github.com>
2024-08-15 17:48:31 +0530

    error

---
+++ 03_BASICS/01_Functions.js
@@ -0,0 +1,24 @@
+function sayMyName() {
+    console.log("S");
+    console.log("H");
+    console.log("U");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    
+}
+
+//sayMyName()
+
+// function addTwoNumbers(number1, number2) {
+
+//     console.log(number1 + number2);
+    
+// }
+
+
+function addTwoNumbers(number1, number2) {
+let result = number1 + number2
+return result
+    // return number1 + number2
+}
